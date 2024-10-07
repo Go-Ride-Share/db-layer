eb7306b01963d05143b85ccac0e4ed7a8b89bb05
Ben Clark <Clarkb2@myumanitoba.ca>
2024-10-07 16:13:59 -0500

    #2 - Add Create Post endpoint - Endpoint Added to DbAccessor

--- DbAccessor/RequestObjects.cs
+++ DbAccessor/RequestObjects.cs
@@ -11,8 +11,13 @@ namespace GoRideShare
         public string PasswordHash { get; set; } = passwordHash;
     }
 
-    public class UserRegistrationInfo(string email, string passwordHash, string name,
-                    string bio, string phoneNumber, string photo)
+    public class UserRegistrationInfo(
+        string email, 
+        string passwordHash, 
+        string name,
+        string bio, 
+        string phoneNumber, 
+        string photo)
     {
         [JsonPropertyName("email")]
         public string Email { get; set; } = email;
@@ -32,4 +37,64 @@ namespace GoRideShare
         [JsonPropertyName("photo")]
         public string Photo { get; set; } = photo;
     }
+
+    public class PostDetails
+    {
+        [JsonPropertyName("postId")]
+        public string PostId { get; set; }
+        
+        [JsonPropertyName("posterId")]
+        public string PosterId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("originLat")]
+        public float OriginLat { get; set; }
+
+        [JsonPropertyName("originLng")]
+        public float OriginLng { get; set; }
+
+        [JsonPropertyName("destinationLat")]
+        public float DestinationLat { get; set; }
+
+        [JsonPropertyName("destinationLng")]
+        public float DestinationLng { get; set; }
+
+        [JsonPropertyName("price")]
+        public float Price { get; set; }
+
+        [JsonPropertyName("seatsAvailable")]
+        public int SeatsAvailable { get; set; }
+
+        public PostDetails() { }
+
+        public PostDetails(
+            string postId,
+            string posterId,
+            string name,
+            string description,
+            float originLat,
+            float originLng,
+            float destinationLat,
+            float destinationLng,
+            float price,
+            int seatsAvailable)
+        {
+            PostId = postId;
+            PosterId = posterId;
+            Name = name;
+            Description = description;
+            OriginLat = originLat;
+            OriginLng = originLng;
+            DestinationLat = destinationLat;
+            DestinationLng = destinationLng;
+            Price = price;
+            SeatsAvailable = seatsAvailable;
+        }
+    }
+
 }
