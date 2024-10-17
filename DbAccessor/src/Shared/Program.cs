b8a36bdcf51cd0cc86d0a38e53216f60703ade54
Asit Patel <patela8@myumanitoba.ca>
2024-10-17 15:07:50 -0500

    refactor folder structure

---
+++ DbAccessor/src/Shared/Program.cs
@@ -0,0 +1,32 @@
+// using Microsoft.Azure.Functions.Worker;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.DependencyInjection;
+
+// var host = new HostBuilder()
+//     .ConfigureFunctionsWebApplication()
+//     .ConfigureServices(services => {
+//         services.AddApplicationInsightsTelemetryWorkerService();
+//         services.ConfigureFunctionsApplicationInsights();
+//     })
+//     .Build();
+
+// host.Run();
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string");
+    Environment.Exit(0);
+}
+
+var client = new MongoClient(connectionString);
+
+var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+
+var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+
+var document = collection.Find(filter).First();
+
+Console.WriteLine(document);
