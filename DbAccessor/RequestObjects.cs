0c05fe10e64fcfbe15e0ed1d2b386ac21be00b15
Asit Patel <patela8@myumanitoba.ca>
2024-10-26 22:59:04 -0500

    clean up code

--- DbAccessor/RequestObjects.cs
+++ DbAccessor/RequestObjects.cs
@@ -228,34 +228,4 @@ namespace GoRideShare
             Contents = contents;
         }
     }
-
-    [BsonIgnoreExtraElements]
-    public class Movie
-    {
-
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
-
-        [BsonElement("title")]
-        [JsonRequired]
-        [JsonPropertyName("title")]
-        public string Title { get; set; } = null!;
-
-        [BsonElement("plot")]
-        [JsonRequired]
-        [JsonPropertyName("plot")]
-        public string Plot { get; set; } = null!;
-
-        public Movie
-        (
-            string title,
-            string plot
-        )
-        {
-            Title = title;
-            Plot = plot;
-        }
-    }
-
 }
